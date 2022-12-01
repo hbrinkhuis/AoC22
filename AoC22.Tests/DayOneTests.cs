4fd806cee17641eb2f95f6b53faf9d61a8213c9f
Henri Brinkhuis <henri@htbr.nl>
2022-12-01 16:37:50 +0100

    DayOneTest with actual (sample) input

--- AoC22.Tests/DayOneTests.cs
+++ AoC22.Tests/DayOneTests.cs
@@ -4,13 +4,43 @@ namespace AoC22.Tests;
 
 public class DayOneTests
 {
+    private const string SampleInput = @"1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+    private readonly IAoCSolver _sut;
+
+    public DayOneTests()
+    {
+        _sut = new DayOneSolver();
+    }
+    
     [Fact]
     public void DayOneTest_NoInput_ShouldThrowArgumentNullException()
     {
-        IAoCSolver solver = new DayOneSolver();
-
-         var action = () => solver.Solve(string.Empty);
+         var action = () => _sut.SolvePartOne(string.Empty);
 
          action.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void DayOneTest_PartOneSampleInput_ShouldReturnAnswer()
+    {
+        const string expected = "24000";
+
+        var result = _sut.SolvePartOne(SampleInput);
+
+        result.Should().Be(expected);
     }
 }
