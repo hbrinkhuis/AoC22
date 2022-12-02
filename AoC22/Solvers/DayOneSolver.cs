41b57a51f309cc03e30c7af50115f28513f7013c
Henri Brinkhuis <henri@htbr.nl>
2022-12-02 14:33:54 +0100

    Solves part one (at least using the example values)

--- AoC22/Solvers/DayOneSolver.cs
+++ AoC22/Solvers/DayOneSolver.cs
@@ -2,10 +2,41 @@
 
 public class DayOneSolver : IAoCSolver
 {
+    private struct Elve
+    {
+        public int CaloricValue;
+
+        public Elve()
+        {
+            CaloricValue = 0;
+        }
+    }
     public string SolvePartOne(IEnumerable<string> input)
     {
         if (input == null || !input.Any())
             throw new ArgumentNullException(nameof(input));
-        return string.Empty;
+
+        var elves = new List<Elve>();
+        var currentElve = new Elve();
+        foreach (var line in input)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                elves.Add(currentElve);
+                currentElve = new Elve();
+                
+            }
+            else
+            {
+                if (int.TryParse(line, out var result))
+                {
+                    currentElve.CaloricValue += result;
+                }
+            }
+        }
+
+        var maxCaloricValue = elves.MaxBy(c => c.CaloricValue).CaloricValue;
+
+        return maxCaloricValue.ToString();
     }
 }
