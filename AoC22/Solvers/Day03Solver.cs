418ae053f0d3c5b4164acff818644b7ed718c478
Henri Brinkhuis <henri@htbr.nl>
2022-12-07 15:55:01 +0100

    Solved Day 3 part 1

--- AoC22/Solvers/Day03Solver.cs
+++ AoC22/Solvers/Day03Solver.cs
@@ -1,14 +1,38 @@
-﻿namespace AoC22.Solvers;
+﻿using System.Text;
+
+namespace AoC22.Solvers;
 
 public class Day03Solver : IAoCSolver
 {
     public string SolvePartOne(string[] input)
     {
-        throw new NotImplementedException();
+        var totalValue = 0;
+        foreach (var line in input)
+        {
+            var compartmentLength = line.Length / 2;
+            var firstCompartment = line[..compartmentLength];
+            var secondCompartment = line[compartmentLength..];
+
+            var duplicateItem = firstCompartment.First(secondCompartment.Contains);
+            totalValue += CalculateValueOfDuplicateItem(duplicateItem);
+        }
+
+        return totalValue.ToString();
     }
 
     public string SolvePartTwo(string[] input)
     {
         throw new NotImplementedException();
+    }
+
+    private static int CalculateValueOfDuplicateItem(char duplicate)
+    {
+        var tableValue = (int) duplicate;
+        return tableValue switch
+        {
+            >= 97 and <= 122 => tableValue - 96,
+            >= 65 and <= 90 => tableValue - 38,
+            _ => 0
+        };
     }
 }
